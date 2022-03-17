ca95016aa0a703daa599bd8f7b3ed9610e65fd7d
cnysten <cnysten@student.hive.fi>
2022-03-17 13:10:47 +0200

    opens game window

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pygame
 
-WINDOW_HEIGHT = 600
-WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 750
+WINDOW_WIDTH = 750
 BLOCK_SIZE = 150
-
+GREEN = (50,205,50)
 
 def draw_grid():
     global WINDOW_HEIGHT, WINDOW_WIDTH, BLOCK_SIZE
-    
 
 def main():
     global SCREEN, CLOCK
     pygame.init()
     SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill(GREEN)
+
+    while True:
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
