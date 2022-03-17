dcc0dd2d6b2ccf90ce703b0634e5690b0ee14828
cnysten <cnysten@student.hive.fi>
2022-03-17 13:00:51 +0200

    main.py added

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 600
+BLOCK_SIZE = 150
+
+
+def draw_grid():
+    global WINDOW_HEIGHT, WINDOW_WIDTH, BLOCK_SIZE
+    
+
+def main():
+    global SCREEN, CLOCK
+    pygame.init()
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    
+
+if __name__ == "__main__":
+    main()
